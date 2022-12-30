7277a5b11196232458457d7ab71a11366b5b344b
tyler.hodgkins <tyler.hodgkins@workday.com>
2022-12-29 23:56:30 -0800

    2022 Optimize Day 19

--- 2022/src/main/kotlin/advent/Day19.kt
+++ 2022/src/main/kotlin/advent/Day19.kt
@@ -1,23 +1,21 @@
 package advent
 
 import advent.Day19.Resource.*
+import kotlin.math.ceil
 import kotlin.math.max
 
 object Day19 : AdventDay {
+
     override fun part1(input: List<String>): Any {
         val blueprints = parseInput(input)
-        val results = blueprints.map {
-            it.simulateOptimalGeodes(24)
-        }
+        val results = blueprints.map { it.simulateOptimalGeodesDfs(24) }
         debugln(results)
         return results.mapIndexed { index, value -> (index + 1) * value }.sum()
     }
 
     override fun part2(input: List<String>): Any {
         val blueprints = parseInput(input.take(3))
-        val results = blueprints.map {
-            it.simulateOptimalGeodes(32)
-        }
+        val results = blueprints.map { it.simulateOptimalGeodesDfs(32) }
         debugln(results)
         return results.reduce(Int::times)
     }
@@ -26,10 +24,14 @@ object Day19 : AdventDay {
         Ore,
         Clay,
         Obsidian,
-        Geode
+        Geode;
+
+        companion object {
+            val none: Map<Resource, Int> = Resource.values().associateWith { 0 }
+        }
     }
 
-    val resourcesToBuy: List<Resource> = Resource.values().toList()
+    val resourcesToBuy: List<Resource> = Resource.values().toList().reversed()
 
     data class Robot(val produces: Resource, val cost: Map<Resource, Int>)
     data class Blueprint(val robots: Map<Resource, Robot>) {
@@ -37,81 +39,70 @@ object Day19 : AdventDay {
         val maxCost: Map<Resource, Int> =
             Resource.values().associateWith { res -> (robots.values.mapNotNull { it.cost[res] }).maxOrNull() ?: 0 }
 
-        fun simulateOptimalGeodes(duration: Int): Int {
-            var currentState = sequenceOf(
-                SimulationState(
-                    duration,
-                    this,
-                    Resource.values().associateWith { 0 },
-                    mapOf(Ore to 1, Clay to 0, Obsidian to 0, Geode to 0),
-                    Ore
-                ),
-                SimulationState(
-                    duration,
-                    this,
-                    Resource.values().associateWith { 0 },
-                    mapOf(Ore to 1, Clay to 0, Obsidian to 0, Geode to 0),
-                    Clay
-                )
-            )
-            val resolvedStates = mutableListOf<SimulationState>()
-            while (currentState.any()) {
-                val maxUnresolvedGeodes = currentState.maxOfOrNull { it.resources[Geode]!! } ?: 0
-                val maxResolvedGeodes = resolvedStates.maxOfOrNull { it.resources[Geode]!! } ?: 0
-//                val averageGeodes = unresolved.sumOf { it.resources[Geode]!! } / unresolved.count()
-                debugln("Time remaining: ${currentState.first().timeRemaining}, Possible states: ${resolvedStates.count()} resolved / ${currentState.count()} active, Maximum geodes: ${currentState.maxBy { it.resources[Geode]!! }.resources[Geode]!!}")
-                val nextState = currentState.filter {
-                    val maxPossibleGeodes = it.maxPossibleGeodes()
-                    maxPossibleGeodes > max(maxResolvedGeodes, maxUnresolvedGeodes)
-                }.flatMap { it.action() }.distinct().toList().asSequence()
+        fun costOf(robotType: Resource): Map<Resource, Int> = robots[robotType]!!.cost
 
-                resolvedStates += nextState.filter { it.timeRemaining == 0}
-                currentState = nextState.filter { it.timeRemaining != 0 }.toList().asSequence()
-                // Beware Ye Hacky Optimization
-                val maxBotsPerType = Resource.values().associateWith { res -> currentState.maxOfOrNull { it.robots[res]!! } ?: 0 }
-                currentState = currentState.filter {
-                    when {
-                        maxBotsPerType[Geode]!! > 0 -> it.robots[Geode]!! >= maxBotsPerType[Geode]!! - 1
-                        maxBotsPerType[Obsidian]!! > 0 -> it.robots[Obsidian]!! >= maxBotsPerType[Obsidian]!! - 2
-                        maxBotsPerType[Clay]!! > 0 -> it.robots[Clay]!! >= maxBotsPerType[Clay]!! - 3
-                        else -> true
-                    }
-                }.toList().asSequence()
+        fun simulateOptimalGeodesDfs(duration: Int): Int {
+            val initialState = SimState(
+                duration,
+                this,
+                Resource.none,
+                mapOf(Ore to 1, Clay to 0, Obsidian to 0, Geode to 0),
+                null
+            )
+            var maxGeodes = SimState(0, this, Resource.none, mapOf(), null)
+            depthFirstSearch(initialState) { state ->
+                state.action().filter {
+                    if (it.timeRemaining == 0) {
+                        if (it.resources[Geode]!! > maxGeodes.resources[Geode]!!) {
+                            maxGeodes = it
+                            debugln("Candidate found: ${maxGeodes.resources[Geode]}")
+                        }
+                        false
+                    } else it.maxPossibleGeodes() > maxGeodes.resources[Geode]!!
+                }
             }
-            return resolvedStates.maxOf { it.resources[Geode]!! }
+
+            debugln(maxGeodes.toString(true), 2)
+            return maxGeodes.resources[Geode]!!
         }
     }
 
-    private data class SimulationState(
+    private data class SimState(
         val timeRemaining: Int,
         val blueprint: Blueprint,
         val resources: Map<Resource, Int>,
         val robots: Map<Resource, Int>,
-        val toBuy: Resource,
+        val priorState: SimState?
     ) {
-        fun canAfford(type: Resource): Boolean =
-            blueprint.robots[type]!!.cost.all { (resource, cost) -> resources[resource]!! >= cost }
+        fun toString(deep: Boolean): String {
+            val builder = StringBuilder(if (deep) priorState?.toString(true) + "\n" else "")
+            builder.appendLine("===============")
+            builder.appendLine("$timeRemaining time left")
+            Resource.values().forEach {
+                builder.appendLine("    $it -> ${this.resources[it]} (+${this.robots[it]}) next: ${timeToAffordNextRobot(it)}")
+            }
+            return builder.toString()
+        }
 
         fun timeToAffordNextRobot(type: Resource): Int =
-            blueprint.robots[type]!!.cost.minOf { (resource, cost) ->
-                if (robots[resource]!! > 0) {
-                    max(cost - resources[resource]!!, 0) / robots[resource]!!
+            blueprint.robots[type]!!.cost.maxOf { (resource, cost) ->
+                if ((robots[resource] ?: 0) > 0) {
+                    ceil(max(cost - resources[resource]!!, 0) / robots[resource]!!.toFloat()).toInt() + 1
                 } else {
-                    -1
+                    Int.MAX_VALUE
                 }
             }
 
-        fun fastForward(): SimulationState = this.copy(
+        fun fastForward(): SimState = this.copy(
             timeRemaining = 0,
             blueprint = blueprint,
             resources = resources.mapValues { (res, value) -> value + robots[res]!! * timeRemaining },
             robots = robots,
-            toBuy = toBuy
         )
 
         fun maxPossibleGeodes(): Int {
             val base = this.resources[Geode]!! + (this.robots[Geode]!! * timeRemaining)
-            var n = timeRemaining - timeToAffordNextRobot(toBuy)
+            var n = timeRemaining
             var extraBots = 0
             var extraGeodes = 0
             while (n > 0) {
@@ -122,50 +113,28 @@ object Day19 : AdventDay {
             return base + extraGeodes
         }
 
-        fun action(): List<SimulationState> {
-            if (timeRemaining == 0) return listOf(this)
-            val newResources = resources.mapValues { (res, value) -> value + robots[res]!! }
-            if (timeRemaining == 1) return listOf(this.copy(
-                timeRemaining = 0,
-                resources = newResources
-            ))
-            return if (canAfford(toBuy)) {
-                val cost = blueprint.robots[toBuy]!!.cost
-                val resourcesAfterPurchase = newResources.mapValues { (res, value) ->
-                    value - (cost[res] ?: 0)
+        fun action(): List<SimState> {
+            return resourcesToBuy.map { it to timeToAffordNextRobot(it) }
+                .filter { (type, timeToNext) ->
+                    // Only add if we will be able to afford and we need more to maximize throughput
+                    timeToNext < this.timeRemaining && (type == Geode || blueprint.maxCost[type]!! > robots[type]!!)
                 }
-                val robotsAfterPurchase =
-                    robots.mapValues { (type, count) -> if (type == toBuy) count + 1 else count }
-                resourcesToBuy.filter {
-                    // Only add if we need more to maximize throughput
-                    it == Geode || blueprint.maxCost[it]!! > robotsAfterPurchase[it]!!
-                }.map { newToBuy ->
-                    val temp = this.copy(
-                        timeRemaining = timeRemaining - 1,
+                .map { (type, timeToNext) ->
+                    val newResources = resources.mapValues { (res, value) -> value + robots[res]!! * timeToNext }
+                    val cost = blueprint.costOf(type)
+                    val resourcesAfterPurchase = newResources.mapValues { (res, value) -> value - (cost[res] ?: 0) }
+                    val robotsAfterPurchase = robots.mapValues { (it, count) -> if (it == type) count + 1 else count }
+                    this.copy(
+                        timeRemaining = timeRemaining - timeToNext,
                         resources = resourcesAfterPurchase,
                         robots = robotsAfterPurchase,
-                        toBuy = newToBuy,
+                        priorState = this
                     )
-
-                    val timeToAffordNextRobot = temp.timeToAffordNextRobot(temp.toBuy)
-                    when {
-                        timeToAffordNextRobot < 0 -> temp.fastForward()
-                        temp.toBuy == Geode && timeToAffordNextRobot < timeRemaining -> temp
-                        temp.toBuy == Clay && timeToAffordNextRobot < timeRemaining - 2 -> temp
-                        timeToAffordNextRobot in 0 until timeRemaining - 2 -> temp
-                        else -> {
-                            temp.fastForward()
-                        }
-                    }
+                }.ifEmpty {
+                    // Can't afford any more robots before time runs out. Skip to the end.
+                    listOf(this.fastForward())
                 }
-            } else {
-                listOf(this.copy(
-                    timeRemaining = timeRemaining - 1,
-                    resources = newResources,
-                ))
-            }
         }
-
     }
 
     private val lineRegex = Regex("Blueprint \\d+: Each ore robot costs (\\d+) ore. Each clay robot costs (\\d+) ore. Each obsidian robot costs (\\d+) ore and (\\d+) clay. Each geode robot costs (\\d+) ore and (\\d+) obsidian.")
