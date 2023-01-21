ec0786bda929dd2e19eed8b31354452fa8b59885
Tyler Hodgkins <ty1824@gmail.com>
2023-01-21 12:05:22 -0800

    2022 Add Application plugin to gradle build

--- 2022/build.gradle.kts
+++ 2022/build.gradle.kts
@@ -1,15 +1,21 @@
 plugins {
     idea
     kotlin("jvm")
+    application
 }
 
 repositories {
     mavenCentral()
     google()
 }
+
 dependencies {
     implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+application {
+    mainClass.set(advent.AdventRunnerKt)
 }
